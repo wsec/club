3253996f79458bfbba66776bfc3cf3ba91183a51
Jennings Zhang <jzhg2000@gmail.com>
2017-10-08 14:36:51 -0400

    Update bad-cygwin.sh to include gentoo-bashrc

--- student_resources/bad-cygwin.sh
+++ student_resources/bad-cygwin.sh
@@ -4,7 +4,7 @@
 #This script will make the cygwin linux terminal emulation program on school computers less frustrating to use. 
 #Usage: ./bad-cygwin.sh [NEW_HOME_DIR]
 #WARNING: this script was kinda inflexible... This software is provided AS IS, don't blame me for any catastrophies or vomitting.
-#version 0.3
+#version 0.4
 
 #Features overview: 
 #launch notepad++ from cygwin simply by typing "npp &".
@@ -16,14 +16,13 @@
 
 TODAY=$(date "+%m/%d/%Y")
 echo "Today is $TODAY"
-echo "This script will make cygwin on a school computer less frustrating to use. Written by Jennings Zhang, May 2017."
+echo "This script will make cygwin on a school computer less frustrating to use. Written by Jennings Zhang, last updated October 2017."
 
 if [[ "$1" = *-h* ]]; then
   
   echo "Usage: ./bad-cygwin.sh [NEW_HOME_DIR]"
   exit 0
 fi
-
 
 cd ~
 
@@ -32,6 +31,7 @@ cp -v .bash_profile .bash_profile.backup
 new_home="/cygdrive/h/cygwin_home"
 new_home_esc="\/cygdrive\/h\/cygwin_home\/"
 
+#in case a specific new home directory was specified by $1
 if [ -n "$1" ]; then
   $new_home=$1
   new_home_esc=$(echo $new_home | sed 's/\//\\\//g')
@@ -39,30 +39,32 @@ fi
 
 mkdir $new_home
 sed -i "1iexport HOME=$new_home_esc\ncd ~" .bash_profile
-#regex=""
-#new_home="export HOME=\/cygdrive\/c\/cygwin_home\/\ncd ~"
-#sed  "/$regex/ { N; s/$regex\n/$new_home\n&/ }" .bash_profile > buffer && mv -f buffer .bash_profile
+
 echo "Your new home (~) directory is: $new_home. Cygwin will now open in this directory by default." 
 
-cp -vi .bashrc .inputrc .profile $new_home
+ARRAY=(.bashrc .inputrc .profile)
+
+cp -vi ${ARRAY[*]} $new_home
+
+echo "Copied ${ARRAY[*]} into $new_home."
 cd $new_home
 
+#append scripts to .bashrc
+#retrive gentoo-bashrc from Github repository
+#also add some useful functions and aliases
 cat >> .bashrc << EOF
-#>>>>BEGIN added by bad-cygwin.sh on $TODAY
-alias ls="ls --color=auto"
-alias grep="grep --color"
-alias egrep="egrep --color=auto"
-alias fgrep="fgrep --color=auto"
-# don't put duplicate lines or lines starting with space in the history.
-# See bash(1) for more options
-HISTCONTROL=ignoreboth
-# append to the history file, don't overwrite it
-shopt -s histappend
-# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=100
-HISTFILESIZE=2000
+
+#<<<<<<< BEGIN added by bad-cygwin.sh on $TODAY
+
+#This is the bashrc file from gentoo linux.
+$(curl https://raw.githubusercontent.com/jennydaman/twlinux/master/student_resources/gentoo-bashrc)
+
+#add return status before bash prompt
+PS1='$(EXIT="$?"; [ "$EXIT" -ne "0" ] && echo "\[\e[31;1m\]$EXIT|")'$PS1
+
+#Here are the customizations specific to cygwin use in prog 3:
+
 alias npp="/cygdrive/c/Program\ Files\ \(x86\)/npp/notepad++.exe"
-export PS1='\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w$(EXIT="$?"; [ "$EXIT" -ne "0" ] && echo "\[\e[31;1m\] | $EXIT")\[\e[0m\]\n\$ '
 
 createc() {
   local file_name=hello_world.c
@@ -79,23 +81,25 @@ int main(int argc, char *argv[]) {
 }
 ENDOFHELLOWORLD
 }
-#<<<<END added by bad-cygwin.sh
+
+#>>>>>>> END added by bad-cygwin.sh
 EOF
 
 cat >> .virc << EOF
-set backspace=indent,eol,start
 set number
 syntax on
 set cursorline
 hi CursorLine term=bold cterm=bold 
 EOF
 
-echo "Some lines were added to the ~/.bashrc configuration file."
+#clean up, clean up, everybody everywhere
+unset TODAY new_home new_home_esc ARRAY
+
+echo "Some lines were added to the $(tput bold)~/.bashrc$(tput sgr0) configuration file."
 echo "I've made some quality of life changes."
 echo 'createc [FILENAME] will write "Hello World" out to the specified file.'
-echo 'npp [FILENAME] & will launch notepad++ directly from cygwin.'
-echo 'Tip: "&" will make a job run in the background. If you launch notepad++ without the "&", then it will keep your shell busy.'
+echo '$(tput bold)npp [FILENAME] &$(tput sgr0) will launch notepad++ directly from cygwin.'
 
-echo "Done! Please restart cygwin to fully apply the changes. "
+echo "Done! Please $(tput bold)restart cygwin$(tput sgr0) to finish applying the changes. "
 echo "If you want to revert these changes, delete the cygwin_home folder, then: cd /home/\$USER && mv .bash_profile.backup .bash_profile"
 echo "DFTBA"
